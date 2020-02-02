7b0371354a4e39dd7a6c6ad96f714bb9e0022aaf
SyedAllay <syedallay.ali@ryerson.ca>
2020-02-01 21:34:36 -0500

    .

--- firstGame.py
+++ firstGame.py
@@ -13,3 +13,33 @@ screenSize = (800,800)
 screen = pygame.display.set_mode(screenSize)
 # initialize what is writeen at the top of the title-bar 
 pygame.display.set_caption('My First Game')
+
+# condition
+carryOn = True
+
+# clock will be used to update how quickly screen updates (FPS)
+clock = pygame.time.Clock()
+
+# Main game loop, while carryOn is True, game will run 
+while carryOn:
+	# For loop get all events that happen
+	for event in pygame.event.get():
+		# pygame.QUIT is the event where you press x on the top left of title-bar
+		if event.type == pygame.QUIT:
+			carryOn = False 
+			
+	# GAME LOGIC
+
+
+
+	# DRAWING STUFF 
+
+	screen.fill(black)
+	pygame.draw.rect(screen, red, [55, 200, 100, 70],0)
+
+	## UPDATE SCREEN AND FPS  
+	pygame.display.flip()
+	clock.tick(60)
+
+# exit game engine after you exit the game 
+pygame.quit()
