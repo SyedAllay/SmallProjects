35186a253f47c4d07d5938715b7a137efc2b0939
SyedAllay <syedallay.ali@ryerson.ca>
2020-02-03 09:25:01 -0500

    firstGame.py

--- firstGame.py
+++ firstGame.py
@@ -11,7 +11,7 @@ red = (255,0,0)
 # initialize other variables  
 screenSize = (800,600)
 size = 50 
-
+score = 0 
 
 # display screen
 screen = pygame.display.set_mode(screenSize)
@@ -36,85 +36,87 @@ class enemyWall:
         self.size = size
 
     def moveR(self):
-    	if self.x1 <= self.screenWidth:
-    		self.x1 += 5
-    	else:
-    		self.x1 = -self.size*2
+        if self.x1 <= self.screenWidth:
+            self.x1 += 5
+        else:
+            self.x1 = -self.size*2
 
     def moveL(self):
-    	if self.x1 >= -self.size*2:
-    		self.x1 -= 5
-    	else:
-    		self.x1 = self.screenWidth + self.size*2
+        if self.x1 >= -self.size*2:
+            self.x1 -= 5
+        else:
+            self.x1 = self.screenWidth + self.size*2
 
     def moveRan(self):
-    	if self.y == -self.size:
-    		self.x1 = random.randint(0,self.screenWidth)
-    	if self.y <= self.screenHeight:
-    		self.y += 5
-    	else:
-    		self.y = -self.size
+        if self.y == -self.size:
+            self.x1 = random.randint(0,self.screenWidth-self.size*2)
+        if self.y <= self.screenHeight:
+            self.y += 5
+        else:
+            self.y = -self.size
 
     def draw(self):
-    	EnemyRec = pygame.draw.rect(screen, (0,255,0), (self.x, self.y, self.screenWidth, self.size)) 
-    	EnemyRecHole = pygame.draw.rect(screen, (0,0,0), (self.x1, self.y, self.size + 50, self.size + 50)) 
+        EnemyRec = pygame.draw.rect(screen, (0,255,0), (self.x, self.y, self.screenWidth, self.size)) 
+        EnemyRecHole = pygame.draw.rect(screen, (0,0,0), (self.x1, self.y, self.size + 50, self.size + 50)) 
 
 # create player objct 
 class player: 
 
-	def __init__(self,x,y,width,height):
-		self.x = x
-		self.y = y
-		self.width = width
-		self.height = height
-		self.speed = 10
+    def __init__(self,x,y,width,height):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.speed = 10
+    def ok(self):
+        print('lol')        
 
-	def move(self):
-		keys = pygame.key.get_pressed() 
-		# K_[direction] are the arrow keys, if statements retrieves if keys are being pressed down as either True or False 
-		if keys[pygame.K_LEFT]:
-			self.x -= self.speed 
-		if keys[pygame.K_RIGHT]:
-			self.x += self.speed 
-		if keys[pygame.K_UP]:
-			self.y -= self.speed 
-		if keys[pygame.K_DOWN]:
-			self.y += self.speed 
-		# Contrain movement so player can't leave screen
-		if self.x >= (screenSize[0]-self.width):
-			self.x = screenSize[0]-self.width
-		if self.y >= (screenSize[1]-self.height):
-			self.y = screenSize[1]-self.height
-		if self.x <= 0:
-			self.x = 0
-		if self.y <= 0:
-			self.y = 0
+    def move(self):
+        keys = pygame.key.get_pressed() 
+        # K_[direction] are the arrow keys, if statements retrieves if keys are being pressed down as either True or False 
+        if keys[pygame.K_LEFT]:
+            self.x -= self.speed 
+        if keys[pygame.K_RIGHT]:
+            self.x += self.speed 
+        if keys[pygame.K_UP]:
+            self.y -= self.speed 
+        if keys[pygame.K_DOWN]:
+            self.y += self.speed 
+        # Contrain movement so player can't leave screen
+        if self.x >= (screenSize[0]-self.width):
+            self.x = screenSize[0]-self.width
+        if self.y >= (screenSize[1]-self.height):
+            self.y = screenSize[1]-self.height
+        if self.x <= 0:
+            self.x = 0
+        if self.y <= 0:
+            self.y = 0
 
-	def draw(self):
-		# draw player 
-		playerRect = pygame.draw.rect(screen, (255,0,0), (self.x, self.y, self.width, self.height))   
+    def draw(self):
+        # draw player 
+        playerRect = pygame.draw.rect(screen, (255,0,0), (self.x, self.y, self.width, self.height))   
 
 player1 = player(0,0,50,50)
 enemy = enemyWall(screenSize,size)
 # Main game loop, while carryOn is True, game will run 
 while carryOn:
-	# For loop get all events that happen
-	for event in pygame.event.get():
-		# pygame.QUIT is the event where you press x on the top left of title-bar
-		if event.type == pygame.QUIT:
-			carryOn = False 
-	# black background 
-	screen.fill(black)
+    # For loop get all events that happen
+    for event in pygame.event.get():
+        # pygame.QUIT is the event where you press x on the top left of title-bar
+        if event.type == pygame.QUIT:
+            carryOn = False 
+    # black background 
+    screen.fill(black)
 
-	# draw player,enemy and allow for movement
-	drawEnemy = enemy.draw()
-	moveEnemy = enemy.moveR()
-	draw = player1.draw()
-	movement = player1.move()
+    # draw player,enemy and allow for movement
+    drawEnemy = enemy.draw()
+    moveEnemy = enemy.moveR()
+    draw = player1.draw()
+    movement = player1.move()
 
-	## UPDATE SCREEN AND FPS  
-	pygame.display.update()
-	clock.tick(60)
+    ## UPDATE SCREEN AND FPS  
+    pygame.display.update()
+    clock.tick(60)
 
 # exit game engine after you exit the game 
 pygame.quit()
